7bee52b03a61011cc905deea678f30120accc722
deanna-yee <deannayee0108@gmail.com>
2019-07-19 16:14:28 -0700

    update Board.py

--- Board.py
+++ Board.py
@@ -3,7 +3,6 @@ from copy import deepcopy
 class Board:
     def __init__(self):
         self.board = []
-<<<<<<< HEAD
         self.rows = 6
         self.columns = 7
         for r in range(self.rows):
@@ -13,8 +12,3 @@ class Board:
 
     def copyBoard(self):
         return deepcopy(self.board)
-=======
-        for _ in range(6):
-            for _ in range(7):
-                
->>>>>>> 66ba5311eaf429b5210f4e3a121e1a25cca585eb
