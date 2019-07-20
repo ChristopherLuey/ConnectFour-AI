66ccdca6c7748461fa5cf0c1f72253166f10fddd
Chrisotpher Luey <cluey21@csus.org>
2019-07-20 13:34:11 -0700

    alpha

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import player
 
 def main():
     p = player.Player()
+    p2 = player.Player("o")
     board = Board.Board()
-    board.printBoard()
+    win = False
+    turn = p
+
+    while(not win):
+        board.printBoard()
+        col = turn.Col()
+        row = board.findRow(col)
+        board.placePiece(row, col, turn)
+        if(board.win(row, col, turn) == True):
+            win = True;
+        if(turn == p):
+            turn = p2
+        elif(turn == p2):
+            turn = p
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,3 +1,6 @@
 class Player:
     def __init__(self, piece = "X"):
         self.piece = piece
+
+    def Col(self):
+        return int(input("What column to place?"))
